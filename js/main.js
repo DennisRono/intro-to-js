90a5af3525adce65d7176e3e02903975fd7052c3
DennisRono <dennisrkibet@agmail>
2024-06-19 11:24:15 +0300

    assignment let

--- js/main.js
+++ js/main.js
@@ -1,56 +1,32 @@
-// Variables
-/**  var, let, or const */
+// Part 1
+let age = 22
+let name = 'Dennis Kibet'
+let isStudent = true
 
-// Data Types
-/** Number - numerical value */
-let number = 42
-/** string - represents any data inside quotation marks "" */
-let name = 'kibet'
-/** Boolean - yes/no; 0/1 - true/false data type */
-let isHappy = true
-/** Null - Empty value */
-let nullEmptyValue = null
-/** Undefined -  A variable that has been declared but not yet assigned a value.*/
-let unassignedValue
+// Part 2
+let x = 10
+let y = 5
+let sum = x + y
+let product = x * y
+let difference = x - y
+let quotient = x / y
+let remaider = x % y
 
-/** Object  - Key value pair value (blue print info) */
-let person = {
-  firstName: 'Dennis',
-  lastName: 'Kibet',
-  age: 21,
-}
+// Part 3
+let greeting = 'Hello, my name is Dennis Kibet'
+// let fullGreeting = "Hello, my name is " + name + " and I am " + age + " years old."
+let fullGreeting = `Hello, my name is ${name} and I am ${age} years old.`
 
-/** Array - data type for storing lists*/
-let fruits = ['Apple', 'Banana', 'Cherry']
-
-/** Symbol (ES6) - Represents a unique and immutable value that can be used as the key of an object property. */
-let symbol1 = Symbol('description')
-
-/** BigInt (ES2020) - Allows you to represent whole numbers larger than 2^53 - 1, which is the largest number JavaScript can reliably represent with the Number type. */
-let bigIntNumber = BigInt(1234567890123456789012345678901234567890)
-let anotherBigInt = 1234567890123456789012345678901234567890n // Using the 'n' notation
-
-/**
- * Primitive Data Types - that only represent a single value -  is a basic, immutable data type. it hold the actual values and not reference
- */
-
-// Operators
-// Functions
-// Conditional Statements
-// Loops
-// Arrays and Objects
-// Events
-// High Order Functions
-/** .map() */
-;[1, 2, 3, 4, 5].map((i) => {
-  return i * 2
-})
-/** .filter() */
-/** .reduce() */
-/** .forEach() */
-/** .find() */
-/** .some() */
-/** .every() */
-/** .sort() */
-/** .concat() */
-/** .flatMap() */
+// Part 4
+console.log(age)
+console.log(name)
+console.log(isStudent)
+console.log(x)
+console.log(y)
+console.log(sum)
+console.log(product)
+console.log(difference)
+console.log(quotient)
+console.log(remaider)
+console.log(greeting)
+console.log(fullGreeting)
